ece8cc90789132fb6d9095ec9508a8cffc6ddc99
Administrator <827773490@qq.com>
2022-06-19 22:06:55 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/houseprice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.houseprice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:houseprice/coin_data.dart';
+import 'price_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+
+    );
+
+  }
+}
